393e3974527539971bcda9b322077105403d55d1
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-11 16:32:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,11 @@
-
+def speak_to_grandma(speak)
+  if speak == speak.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
+def
 if speak_to_grandma == .uppercase
   puts "NO, NOT SINCE 1938!"
   

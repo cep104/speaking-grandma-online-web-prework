323af73113b8eec6e8e2958fdfd001fe44d11b97
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-11 16:44:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 
 def speak_to_grandma(talk)
-if speak_to_grandma(talk) == talk.uppercase
+if talk == talk.uppercase
   puts "NO, NOT SINCE 1938!"
   
-elsif speak_to_grandma(talk) == "I LOVE YOU GRANDMA!"
+elsif talk == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   
 else

300ebdaa0b4bfdb1b7125a283737fc3532b17928
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-11 16:53:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,11 @@
-
-def speak_to_grandma(talk)
-  if talk == talk.upcase
-  puts "NO, NOT SINCE 1938!"
-  
-elsif talk == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-  
-else
-  puts "HUH?! SPEAK UP, SONNY"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
+
 

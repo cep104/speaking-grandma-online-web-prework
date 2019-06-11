a4ffe4303409a2fd9f4596311367c29cbe147e2b
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-11 16:33:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,8 @@ def speak_to_grandma(speak)
     "HUH?! SPEAK UP, SONNY!"
   end
 end
-def
-if speak_to_grandma == .uppercase
+def speak_to_grandma(talk)
+if speak_to_grandma == talk.uppercase
   puts "NO, NOT SINCE 1938!"
   
 elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
@@ -14,5 +14,6 @@ elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
   
 else
   puts "HUH?! SPEAK UP, SONNY"
+  end
 end
 
